579882ed40bc3423e68aec93ed3e203fa7c9852f
Abraham Daguer <abrahamg1235@gmail.com>
2022-11-09 16:40:37 -0500

    MatrixGenerator fix

--- Assets/Scripts/Hole/HoleSystem.cs
+++ Assets/Scripts/Hole/HoleSystem.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class HoleSystem : MonoBehaviour
 {
+    [SerializeField] private GameObject matrix;
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("BallPLayer"))
         {
             other.gameObject.GetComponent<BallController>().Stop();
             other.gameObject.GetComponent<BallController>().EnterHole();
+            matrix.GetComponent<MatrixGenerator>().EnterTheHole();
             Debug.Log("Hoyo");
         }
     }
--- Assets/Scripts/MarkovThing/MatrixGenerator.cs
+++ Assets/Scripts/MarkovThing/MatrixGenerator.cs
@@ -1,72 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MatrixGenerator : MonoBehaviour
 {
-    public  float[,] matrix1 = new float[3,3];
-    public  float[,] matrix2 = new float[3, 3];
-    public float[,] matrix3 = new float[3, 3];
+    //matrices
+    private float[,] matrix1 = new float[3,3];
+    private float[,] matrix2 = new float[3, 3];
+    private float[,] matrix3 = new float[3, 3];
+    private float[,] matrixEscogida = new float[3,3];
+
+    private  float t,a,b;
+    private  int id;
+
+    [SerializeField] private string planetaActual;
+  
+    
     void Start()
     {
+
+        
+    }
+    public void EnterTheHole()
+    {
+        ChooseMatrix();
+        ActualPlanet(planetaActual);
+    }
+    private void ChooseMatrix()
+    {
         // primera matrix
+        //tierra
         matrix1[0, 0] = 0.4f;
         matrix1[0, 1] = 0.3f;
         matrix1[0, 2] = 0.3f;
+        //marte
         matrix1[1, 0] = 0.2f;
         matrix1[1, 1] = 0.5f;
         matrix1[1, 2] = 0.3f;
+        //jupiter
         matrix1[2, 0] = 0.5f;
         matrix1[2, 1] = 0.5f;
         matrix1[2, 2] = 0;
+
         // Segunda matrix
+        //tierra
         matrix2[0, 0] = 0.2f;
         matrix2[0, 1] = 0.5f;
         matrix2[0, 2] = 0.3f;
+        //marte
         matrix2[1, 0] = 0.5f;
         matrix2[1, 1] = 0.4f;
         matrix2[1, 2] = 0.1f;
+        //jupiter
         matrix2[2, 0] = 0f;
         matrix2[2, 1] = 0.4f;
         matrix2[2, 2] = 0.6f;
+
         // tercera matrix
+        //tierra
         matrix3[0, 0] = 0.9f;
         matrix3[0, 1] = 0.1f;
         matrix3[0, 2] = 0;
+        //marte
         matrix3[1, 0] = 0.2f;
         matrix3[1, 1] = 0.6f;
         matrix3[1, 2] = 0.2f;
+        //jupiter
         matrix3[2, 0] = 0.3f;
         matrix3[2, 1] = 0.3f;
         matrix3[2, 2] = 0.4f;
-        //for (int i = 0; i < matrix.GetLength(0); i++)
-        //{
 
-        //    for(var j = 0; j < matrix.GetLength(1); j++)
-        //    {
-        //        matrix[i, j] = Random.Range(0f, 1f); 
+        id = Random.Range(1, 4);
+        print(id);
 
-        //    }
-        //}
-
-        for (int i = 0; i < matrix2.GetLength(0); i++)
+        if (id == 1)
         {
-            for (var j = 0; j < matrix2.GetLength(1); j++)
-            {
-                print(i + "," + j + " = " + matrix2[i, j]);
+            matrixEscogida = matrix1;
+        }
+        if (id == 2)
+        {
+            matrixEscogida = matrix2;
+        }
+        if (id == 3)
+        {
+            matrixEscogida = matrix3;
+        }
+    }
+    private void ActualPlanet(string planeta)
+    {
 
-            }
+        if (planeta == "tierra")
+        {
+            ChangeScenes(0);
         }
 
+        if (planeta == "marte")
 
+        {
+            ChangeScenes(1);
+        }
 
+        if (planeta == "jupiter")
+        {
+            ChangeScenes(2);
+        }
     }
+    
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeScenes(int i)
     {
+        t = Random.Range(0, 1f);
+        a = matrixEscogida[i, 0] + matrixEscogida[i, 1];
         
+        print(t);
+        print("a:" + a);
+        print("b:" + b);
+
+        if (t <= matrixEscogida[i, 0])
+        {
+            SceneManager.LoadScene(i);
+            print("primero");
+        }
+        if (t > matrixEscogida[i, 0] && t <= a)
+        {
+            SceneManager.LoadScene(i);
+            print("segundo");
+        }
+
+        if (t > a && t < 1)
+        {
+            SceneManager.LoadScene(i);
+            print("tercero");
+        }
     }
+
 
    
 }
